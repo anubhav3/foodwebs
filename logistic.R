cf46d3c6603739f5fe91e3cf8cb1956eb1a53eea
Owen Petchey <owen.petchey@ieu.uzh.ch>
2018-09-12 17:24:15 +0200

    logistic working better now
    I changed to using log body sizes, and some other things that may have helped.

--- logistic.R
+++ logistic.R
@@ -17,15 +17,16 @@ ggplot(mapping=aes(x=x, y=logistic(x, 1, 1))) +
 ## parameters
 mass_mean <- 1
 mass_sd <- 1
-num_species <- 10000
-real_a <- 1 # ~Real Parameters
+num_species <- 100
+real_a <- 10 # ~Real Parameters
 real_b <- -5 # ~Real Parameters
 
 
 
-x <- rlnorm(num_species, mass_mean, mass_sd) # ~Real Body Sizes
+x <- log10(rlnorm(num_species, mass_mean, mass_sd)) # ~Real Body Sizes
+real_b <- real_b - mean(x)
 # ~Real food web
-p_interaction <- logistic(x, real_a,real_b) 
+p_interaction <- logistic(x, real_a, real_b) 
 
 # ~Stochasticity in the system's biology
 interaction <- numeric(length=num_species)
@@ -59,22 +60,28 @@ ggplot(mapping=aes(x=x, y=p_interaction)) +
 
 
 #Parameter Estimation (ABC)
-numm <- 10000
-guesses <- data.frame(a=runif(numm,-5,5),
+numm <- 100000
+guesses <- data.frame(a=runif(numm,0,1000),
                       b=runif(numm,-10,10),
                       Q = rep(NA,numm))
 
 for(i in 1:numm)
 {
-  guesses$Q[i] <-  -sum(dbinom(obs_interaction, 1, logistic(x, a[i], b[i]), log = TRUE))
+  guesses$Q[i] <-  -sum(dbinom(obs_interaction, 1, logistic(x, guesses$a[i], guesses$b[i]), log = TRUE))
 }
+
+## Why so many infinite values?
+
+guesses <- filter(guesses, is.finite(Q))
 
 ggplot(gather(guesses), aes(x=value)) +
   facet_wrap(~key, scales="free_x") +
   geom_histogram()
 
+threshold <- 100
+
 guesses %>%
-  filter(Q<150) %>%
+  filter(Q<threshold) %>%
   gather() %>%
   ggplot(aes(x=value)) +
      facet_wrap(~key, scales="free_x") +
@@ -82,7 +89,7 @@ guesses %>%
 
 
 means <- guesses %>%
-  filter(Q<50) %>%
+  filter(Q<threshold) %>%
   summarise_all(mean)
   
 
@@ -91,8 +98,11 @@ ggplot(mapping=aes(x=x, y=p_interaction)) +
   geom_point() + geom_line() +
   geom_point(mapping=aes(y=interaction), col="red", size=3) +
   geom_point(mapping=aes(y=obs_interaction), col="green") +
-  geom_line(aes(y=logistic(x, means$a, means$b)))
+  geom_line(aes(y=logistic(x, means$a, means$b)), col="red", linetype="dashed")
 
 
 m1 <- glm(obs_interaction ~ x, family=binomial)
 summary(m1)
+
+## plot the glm curve
+## plot samples from the posteriors
