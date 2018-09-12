303fd9f7f778589d0fca29776f829eda87168cfe
Owen Petchey <owen.petchey@ieu.uzh.ch>
2018-09-12 14:57:04 +0200

    logistic version update

--- logistic.R
+++ logistic.R
@@ -1,5 +1,7 @@
 library(boot)
 library(ggplot2)
+library(dplyr)
+library(tidyr)
 
 #logistic model y = a*x + b  ~Food Web Model
 logistic <- function(x, a, b){
@@ -15,7 +17,7 @@ ggplot(mapping=aes(x=x, y=logistic(x, 1, 1))) +
 ## parameters
 mass_mean <- 1
 mass_sd <- 1
-num_species <- 10
+num_species <- 10000
 real_a <- 1 # ~Real Parameters
 real_b <- -5 # ~Real Parameters
 
@@ -33,47 +35,64 @@ for(i in 1:num_species)
 ## deterministic biology version
 #interaction <- ifelse(p_interaction<0.3, 0, 1)
 
+
+## add multiple observations per interaction (perhaps body size dependent)
+
+
 ggplot(mapping=aes(x=x, y=p_interaction)) +
   geom_point() + geom_line() +
   geom_point(mapping=aes(y=interaction), col="red")
 
 
 ## add observation error
-p_1_1 <- 0.9
-p_0_0 <- 0.9
+p_1_1 <- 1  ## larger values give lower observation error
+p_0_0 <- 1  ## larger values give lower observation error
 rans <- runif(num_species)
 
 obs_interaction <- ifelse(interaction==1 & rans>p_1_1, interaction-1,
                           ifelse(interaction==0 & rans>p_0_0, interaction+1, interaction))
 
+ggplot(mapping=aes(x=x, y=p_interaction)) +
+  geom_point() + geom_line() +
+  geom_point(mapping=aes(y=interaction), col="red", size=3) +
+  geom_point(mapping=aes(y=obs_interaction), col="green")
 
-noise <- rbinom(1,1,y)
-y <- y + noise
-
-x_y <- data.frame(x=x,y=y)
-# ~Interactions observed by a human
-prob <- 0.3
-n_sample <- as.integer(prob*num)
-obs <- x_y[sample(nrow(x_y), n_sample), ]
 
 #Parameter Estimation (ABC)
 numm <- 10000
-a <- runif(numm,0,5)
-b <- runif(numm,-10,10)
-Q = rep(NA,numm)
-
+guesses <- data.frame(a=runif(numm,-5,5),
+                      b=runif(numm,-10,10),
+                      Q = rep(NA,numm))
 
 for(i in 1:numm)
 {
-  Q[i] <-  sum(abs(obs[,2]-a[i]*obs[,1]-b[i])) 
+  guesses$Q[i] <-  -sum(dbinom(obs_interaction, 1, logistic(x, a[i], b[i]), log = TRUE))
 }
-datta <- data.frame(a=a,b=b)
-p_datta <- datta[Q<100,]
 
-meann = colMeans(p_datta)
+ggplot(gather(guesses), aes(x=value)) +
+  facet_wrap(~key, scales="free_x") +
+  geom_histogram()
 
-# ~Estimated Parameters
-obs_a <- meann[1]
-obs_b <- meann[2]
-plot(x,y)
-lines(x,obs_a*x+obs_b,type='l')
+guesses %>%
+  filter(Q<150) %>%
+  gather() %>%
+  ggplot(aes(x=value)) +
+     facet_wrap(~key, scales="free_x") +
+     geom_histogram()
+
+
+means <- guesses %>%
+  filter(Q<50) %>%
+  summarise_all(mean)
+  
+
+
+ggplot(mapping=aes(x=x, y=p_interaction)) +
+  geom_point() + geom_line() +
+  geom_point(mapping=aes(y=interaction), col="red", size=3) +
+  geom_point(mapping=aes(y=obs_interaction), col="green") +
+  geom_line(aes(y=logistic(x, means$a, means$b)))
+
+
+m1 <- glm(obs_interaction ~ x, family=binomial)
+summary(m1)
